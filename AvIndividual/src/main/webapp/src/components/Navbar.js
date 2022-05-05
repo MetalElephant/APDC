ef77f4f0e7a73abef8c765e693e262f4f29990aa
Alexandre <ams.godinho@campus.fct.unl.pt>
2022-05-05 13:45:08 +0100

    Changing front end files to the correct ones

---
+++ AvIndividual/src/main/webapp/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react"
+import logoProduto from "../images/logo-produto.png";
+import logoEquipa from "../images/logo-equipa.png";
+import { Typography, Grid} from "@mui/material";
+import { Box } from "@mui/system";
+
+export default function Navbar() {
+    return(
+        <nav>
+            <Grid container direction="row" justifycontext="space-between" alignItems="center" bgcolor="#D3D3D3" >
+                    <Grid item xs ={3}>
+                       <Box component="img" src = {logoProduto} width="170px" sx={{ml: "20px"}}  />
+                    </Grid>
+                    <Grid item xs ={6}> 
+                        <Typography variant="h2" align = "center"> Welcome!</Typography>
+                    </Grid>
+                    <Grid item xs ={3}>
+                        <Box component="img" src = {logoEquipa} width="220px" sx={{ml: "auto", mr: "20px", display: "flex"}} />
+                    </Grid>
+            </Grid>
+        </nav>
+    )
+}
---
+++ AvIndividual/src/main/webapp/src/restCalls.js
@@ -0,0 +1,97 @@
+//fetch api to make calls to rest backend
+
+class restCalls {
+
+    constructor(){}
+
+
+    //depois alterar url
+    login (username, password) {
+        return fetch ("https://upbeat-polygon-344116.appspot.com/rest/users/login", {   
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        }).then (function (response) {
+            if (!response.ok) {
+                return response.text().then((text) => {
+                    const error = new Error(text)
+                    error.code = response.status;
+                    throw error
+                })
+            }
+            return response.text()
+        }).then(function (text) {
+            localStorage.setItem('token', text);
+            return text;
+        }) 
+    }
+    
+
+    register (username, password, email, name) {
+        return fetch ("https://upbeat-polygon-344116.appspot.com/rest/users/register/", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password,
+                pwdConfirmation: password,
+                email: email,
+                name: name
+            })
+        }).then (function (response) {
+            if (!response.ok) {
+                return response.text().then((text) => {
+                    const error = new Error(text)
+                    error.code = response.status;
+                    throw error
+                })
+            }
+            return response.text()
+        }).then(function (text) {
+            return text;
+        }) 
+    }
+
+    parcelRegister (owner, parcelName, parcelId, markers, description, groundType, currUsage, prevUsage, area) {
+        return fetch ("https://upbeat-polygon-344116.appspot.com/rest/users/parcel", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                owner: owner,
+                parcelName: parcelName,
+                parcelId: parcelId,
+                points:markers,
+                description: description,
+                groundType: groundType,
+                currUsage: currUsage,
+                prevUsage: prevUsage,
+                area: area
+            })
+        }).then (function (response) {
+            if (!response.ok) {
+                return response.text().then((text) => {
+                    const error = new Error(text)
+                    error.code = response.status;
+                    throw error
+                })
+            }
+            return response.text()
+        }).then(function (text) {
+            return text;
+        }) 
+    }
+
+}
+
+const restCallsExport = new restCalls();
+export default restCallsExport;
+
