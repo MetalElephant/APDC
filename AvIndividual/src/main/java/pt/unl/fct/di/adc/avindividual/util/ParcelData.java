2bdc958735628722abdf8747bc16088355e313f7
Alexandre <ams.godinho@campus.fct.unl.pt>
2022-05-07 17:40:27 +0100

    Parcel storing and listing correctly

--- AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/ParcelData.java
+++ AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/ParcelData.java
@@ -3,15 +3,15 @@ import com.google.cloud.datastore.LatLng;
 
 public class ParcelData {
 	
-	public String owner, parcelName, parcelId, description, groundType, currUsage, prevUsage, area;
-	public LatLng[] points;
+	public String owner, parcelName, parcelRegion, description, groundType, currUsage, prevUsage, area;
+	public LatLng[] markers;
 	
 	public ParcelData() {}
 	
-	public ParcelData(String owner, String parcelName, String parcelId, String description, String groundType, String currUsage, String prevUsage, String area) {
+	public ParcelData(String owner, String parcelName, String parcelRegion, String description, String groundType, String currUsage, String prevUsage, String area) {
 		this.owner = owner;
 		this.parcelName = parcelName;
-		this.parcelId = parcelId;
+		this.parcelRegion = parcelRegion;
 		this.description = description;
 		this.groundType = groundType;
 		this.currUsage = currUsage;
