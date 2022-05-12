1204fc084186f1e77e7cef7e08fbc4ab819a2ea8
Guilherme Pereira <gcn.pereira@campus.fct.unl.pt>
2022-05-12 15:48:33 +0100

    Most of the reward ops, changed how parcel modifications work V3

--- AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/ModifyParcelData.java
+++ AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/ModifyParcelData.java
@@ -1,29 +1,48 @@
 package pt.unl.fct.di.adc.avindividual.util;
 
+import com.google.cloud.datastore.Entity;
+
 public class ModifyParcelData {
-	public String owner, parcelName, parcelRegion, description, groundType, currUsage, prevUsage;
+
+	//Parcel info
+	private static final String PARCEL_NAME = "name";
+	private static final String DESCRIPTION = "description";
+	private static final String GROUND_COVER_TYPE = "ground cover type";
+	private static final String CURR_USAGE = "current usage";
+	private static final String PREV_USAGE = "previous usage";
+
+	public String owner, parcelName, description, groundType, currUsage, prevUsage;
 	
 	public ModifyParcelData() {}
 	
-	public ModifyParcelData(String owner, String parcelName, String parcelRegion, String description, String groundType, String currUsage, String prevUsage) {
+	public ModifyParcelData(String owner, String parcelName, String description, String groundType, String currUsage, String prevUsage) {
 		this.owner = owner;
 		this.parcelName = parcelName;
-		this.parcelRegion = parcelRegion;
 		this.description = description;
 		this.groundType = groundType;
 		this.currUsage = currUsage;
 		this.prevUsage = prevUsage;
 	}
 
-	public boolean isDataValid() {
-		if (this.owner == null || this.parcelName == null || this.parcelRegion == null || this.description == null || this.groundType == null || 
-			this.currUsage == null || this.prevUsage == null)
-			return false;
-
-		if (this.owner.length() == 0 || this.parcelName.length() == 0 || this.parcelRegion.length() == 0 || this.description.length() == 0 || this.groundType.length() == 0 || 
-			this.currUsage.length() == 0 || this.prevUsage.length() == 0)
-			return false;	
-			
-		return true;
+	public void removeNulls(Entity modified) {
+		if(this.parcelName == null) {
+			this.parcelName = modified.getString(PARCEL_NAME);
+		}
+		
+		if(this.description == null) {
+			this.description = modified.getString(DESCRIPTION);
+		}
+		
+		if(this.groundType == null) {
+			this.groundType = modified.getString(GROUND_COVER_TYPE);
+		}
+		
+		if(this.currUsage == null) {
+			this.currUsage = modified.getString(CURR_USAGE);
+		}
+		
+		if(this.prevUsage == null) {
+			this.prevUsage = modified.getString(PREV_USAGE);
+		}
 	}
 }
