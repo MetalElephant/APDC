77604656159b2d98947352414fa085370c2b6ca0
Alexandre <ams.godinho@campus.fct.unl.pt>
2022-05-15 13:05:04 +0100

    +1

--- AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/ModifyParcelData.java
+++ AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/ModifyParcelData.java
@@ -2,13 +2,14 @@ package pt.unl.fct.di.adc.avindividual.util;
 
 public class ModifyParcelData {
 
-	public String owner, parcelName, description, groundType, currUsage, prevUsage;
+	public String owner, parcelName, parcelRegion, description, groundType, currUsage, prevUsage;
 	
 	public ModifyParcelData() {}
 	
-	public ModifyParcelData(String owner, String parcelName, String description, String groundType, String currUsage, String prevUsage) {
+	public ModifyParcelData(String owner, String parcelRegion, String parcelName, String description, String groundType, String currUsage, String prevUsage) {
 		this.owner = owner;
 		this.parcelName = parcelName;
+		this.parcelRegion = parcelRegion;
 		this.description = description;
 		this.groundType = groundType;
 		this.currUsage = currUsage;
