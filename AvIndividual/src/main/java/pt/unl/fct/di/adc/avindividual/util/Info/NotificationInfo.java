5f945852774b1019d289afda13006c9f3f00a01e
Alexandre <ams.godinho@campus.fct.unl.pt>
2022-06-26 15:40:28 +0100

    Notifications are now active

---
+++ AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/util/Info/NotificationInfo.java
@@ -0,0 +1,9 @@
+package pt.unl.fct.di.adc.avindividual.util.Info;
+
+public class NotificationInfo {
+    public String message;
+
+    public NotificationInfo(String message){
+        this.message = message;
+    }
+}
