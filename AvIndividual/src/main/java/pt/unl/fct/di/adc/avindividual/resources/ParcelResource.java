3423d0b5877eeb6f34f48e325024ef392555c249
Alexandre <ams.godinho@campus.fct.unl.pt>
2022-07-12 22:57:13 +0100

    bucket fixed

--- AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/resources/ParcelResource.java
+++ AvIndividual/src/main/java/pt/unl/fct/di/adc/avindividual/resources/ParcelResource.java
@@ -102,8 +102,8 @@ public class ParcelResource {
 
 	//Bucket information
 	private static final String PROJECT_ID = "Land It";
-    private static final String BUCKET_NAME = "land-it.appspot.com";
-    private static final String URL =  "https://storage.googleapis.com/land-it.com/";
+    private static final String BUCKET_NAME = "landit-app.appspot.com";
+    private static final String URL =  "https://storage.googleapis.com/landit-app.com/";
 
 	//Keys
 	private static final String USER = "Utilizador";
@@ -1238,21 +1238,13 @@ public class ParcelResource {
         // Create a point for line segment from p to infinite
         LatLng extreme = LatLng.of(60.0, p.getLongitude());
 
-        // Count intersections of the above line
-        // with sides of polygon
         int count = 0, i = 0;
         do
         {
             int next = (i + 1) % n;
 
-            // Check if the line segment from 'p' to
-            // 'extreme' intersects with the line
-            // segment from 'polygon[i]' to 'polygon[next]'
             if (doIntersect(polygon[i], polygon[next], p, extreme))
             {
-                // If the point 'p' is collinear with line
-                // segment 'i-next', then check if it lies
-                // on segment. If it lies, return true, otherwise false
                 if (orientation(polygon[i], p, polygon[next]) == 0)
                 {
                     return onSegment(polygon[i], p,
@@ -1264,13 +1256,10 @@ public class ParcelResource {
             i = next;
         } while (i != 0);
 
-        // Return true if count is odd, false otherwise
-        return (count % 2 == 1); // Same as (count%2 == 1)
+        return (count % 2 == 1);
     }
 
-	// Given three collinear points p, q, r,
-    // the function checks if point q lies
-    // on line segment 'pr'
+    //Check if point q lies on the line segment 'pr'
     private boolean onSegment(LatLng p, LatLng q, LatLng r)
     {
         if (q.getLatitude() <= Math.max(p.getLatitude(), r.getLatitude()) &&
@@ -1283,11 +1272,7 @@ public class ParcelResource {
         return false;
     }
  
-    // To find orientation of ordered triplet (p, q, r).
-    // The function returns following values
-    // 0 --> p, q and r are collinear
-    // 1 --> Clockwise
-    // 2 --> Counterclockwise
+    //Find orientation of ordered triplet (p, q, r).
     static int orientation(LatLng p, LatLng q, LatLng r)
     {
         double val = (q.getLongitude() - p.getLongitude()) * (r.getLatitude() - q.getLatitude())
@@ -1300,53 +1285,39 @@ public class ParcelResource {
         return (val > 0) ? 1 : 2; // clock or counterclock wise
     }
  
-    // The function that returns true if
-    // line segment 'p1q1' and 'p2q2' intersect.
+    //Check if line segment 'p1q1' and 'p2q2' intersect
     private boolean doIntersect(LatLng p1, LatLng q1, LatLng p2, LatLng q2)
     {
-        // Find the four orientations needed for
-        // general and special cases
         int o1 = orientation(p1, q1, p2);
         int o2 = orientation(p1, q1, q2);
         int o3 = orientation(p2, q2, p1);
         int o4 = orientation(p2, q2, q1);
  
-        // General case
         if (o1 != o2 && o3 != o4)
         {
             return true;
         }
  
-        // Special Cases
-        // p1, q1 and p2 are collinear and
-        // p2 lies on segment p1q1
         if (o1 == 0 && onSegment(p1, p2, q1))
         {
             return true;
         }
  
-        // p1, q1 and p2 are collinear and
-        // q2 lies on segment p1q1
         if (o2 == 0 && onSegment(p1, q2, q1))
         {
             return true;
         }
  
-        // p2, q2 and p1 are collinear and
-        // p1 lies on segment p2q2
         if (o3 == 0 && onSegment(p2, p1, q2))
         {
             return true;
         }
  
-        // p2, q2 and q1 are collinear and
-        // q1 lies on segment p2q2
         if (o4 == 0 && onSegment(p2, q1, q2))
         {
             return true;
         }
  
-        // Doesn't fall in any of the above cases
         return false;
     }
 
